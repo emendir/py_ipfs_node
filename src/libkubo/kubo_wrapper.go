772ec1c0bd4c4d92bae1c13b643017dec0b95b1c
Emendir <dev@emendir.tech>
2025-04-06 07:19:44 +0200

    removed spawnNode

--- src/libkubo/kubo_wrapper.go
+++ src/libkubo/kubo_wrapper.go
@@ -2,14 +2,12 @@ package main
 
 // #include <stdlib.h>
 import "C"
+// 
+// import (
+// 	iface "github.com/ipfs/boxo/coreiface"
+// 	"github.com/ipfs/kubo/core"
+// )
 
-import (
-	iface "github.com/ipfs/boxo/coreiface"
-	"github.com/ipfs/kubo/core"
-)
-
-// This will be set during initialization
-var spawnNodeFunc func(repoPath string) (iface.CoreAPI, *core.IpfsNode, error)
 
 func main() {
 	// Required entry point for buildmode=c-shared
